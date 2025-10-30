578a1ae2fdf229385deef4ca5e19af280a804043
flashtxh <txh2468@gmail.com>
2025-10-30 15:51:57 +0000

    finish

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,47 +1,64 @@
-<script setup>
-    // Import BlogPost component
-    import blogPost from './subcomponents/BlogPost2.vue'
-	import axios from 'axios'
-</script>
-
 <script>
-    export default {
-        data() {
-            return {
-                posts: [] // array of post objects
-            }  
-        },
-        computed: {
-            baseUrl() {
-                if (window.location.hostname == 'localhost')
-                    return 'http://localhost:3000'
-                else {
-                    const codespace_host = window.location.hostname.replace('5173', '3000')
-                    return `https://${codespace_host}`;
-                }
-            }
-        },
-        created() { // created is a hook that executes as soon as Vue instance is created
-            axios.get(`${this.baseUrl}/posts`)
-            .then(response => {
-                // this gets the data, which is an array
-                this.posts = response.data
-                console.log(response.data)
-            })
-            .catch(error => {
-                this.posts = [{ entry: 'There was an error: ' + error.message }]
-            })
-        },
-        methods: {
-            deletePost(id) {
-                // TODO: Complete the delete method
+// Import BlogPost component and axios here so the default export can reference them
+import blogPost from './subcomponents/BlogPost2.vue'
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            posts: [] // array of post objects
+        }
+    },
+    computed: {
+        baseUrl() {
+            if (window.location.hostname == 'localhost')
+                return 'http://localhost:3000'
+            else {
+                const codespace_host = window.location.hostname.replace('5173', '3000')
+                return `https://${codespace_host}`;
             }
         }
+    },
+    created() { // created is a hook that executes as soon as Vue instance is created
+        axios.get(`${this.baseUrl}/posts`)
+        .then(response => {
+            // this gets the data, which is an array
+            this.posts = response.data
+            console.log(response.data)
+        })
+        .catch(error => {
+            this.posts = [{ entry: 'There was an error: ' + error.message }]
+        })
+    },
+    methods: {
+        deletePost(id) {
+            axios.get(`${this.baseUrl}/deletePost`, {
+                params: {
+                    id: id
+                }
+            })  
+            .then(response => {
+                // Handle successful deletion
+                this.posts = this.posts.filter(post => post.id !== id);
+            })
+            .catch(error => {
+                console.error('There was an error deleting the post:', error);
+            }); 
+        }
+    },
+    components: {
+        blogPost
     }
+}
 </script>
 
 <template>
-   <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-
+    <div>
+        <blogPost v-for="post in posts" :key="post.id" :post="post">
+            <!-- inject a Delete button into the BlogPost2 slot -->
+            <div style="text-align:center; margin-top:10px;">
+                <button class="btn btn-primary" @click="deletePost(post.id)">Delete</button>
+            </div>
+        </blogPost>
+    </div>
 </template>
-
--- src/components/subcomponents/BlogPost2.vue
+++ src/components/subcomponents/BlogPost2.vue
@@ -1,13 +1,33 @@
 <script>
-    export default {
-        // Add code if/as necessary
-        // Hint: Add properties subject, entry and mood
+export default {
+    // Accept a `post` object with id, subject, entry, mood
+    props: {
+        post: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        imgLoc() {
+            const mood = (this.post.mood || '').toLowerCase();
+            return `/assets/${mood}.png`;
+        }
     }
+}
 </script>
 
 
 <template>
-    <!-- TODO: add your template code here Use boostrap card --> 
+    <!-- Bootstrap card that displays the post and exposes a slot for actions -->
+    <div class="card" style="width: 18rem;">
+        <img :src="imgLoc" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">Blog {{ post.id }} Subject {{ post.subject }}</h5>
+            <p class="card-text">{{ post.entry }}</p>
+            <!-- Slot where parent can inject buttons (e.g. Delete) -->
+            <slot></slot>
+        </div>
+    </div>
 
 </template>
 
